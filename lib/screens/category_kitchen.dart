84326e7b6c741753287054af121249ea818b91ea
NatashaZettler <natasha.ksf.zettler@gmail.com>
2021-05-26 11:01:58 -0300

    style(Category): include image and text for the category

--- lib/screens/category_kitchen.dart
+++ lib/screens/category_kitchen.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 
 class CategoryKitchen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2
-
-      ), itemBuilder: (context, index){
-        return Text('ERRR');
-      }),
+      body: GridView.builder(
+        itemCount: 2,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 1,
+          ),
+          itemBuilder: (context, index) {
+            return GridTile(
+                footer: Center(child: Text('Bake')),
+                child: InkWell(
+              child: Card(
+                child: Image.asset('assets/images/bake.jpg'),
+              ),
+              onTap: (){
+                
+              },
+            ));
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+      ),
     );
   }
 }
