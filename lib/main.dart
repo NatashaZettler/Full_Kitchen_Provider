34c2a315e9b2876eb4a3fa6441a7ea4accdca8d5
NatashaZettler <natasha.ksf.zettler@gmail.com>
2021-05-26 10:47:53 -0300

    fix(Context): include context in all class, not just kitchen class

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:full_kitchen/screens/kitchen.dart';
 
 void main() {
-  runApp(Kitchen());
+  runApp(MyKitchen());
 }
 
-
-
-
+class MyKitchen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Full Kitchen',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Kitchen());
+  }
+}
--- lib/screens/kitchen.dart
+++ lib/screens/kitchen.dart
@@ -4,44 +4,39 @@ import 'package:full_kitchen/screens/category_kitchen.dart';
 class Kitchen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Full Kitchen',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-          body: Container(
-            width: 1000,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/background.jpg'),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'Bem-vinda(o)',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.white),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => CategoryKitchen(),
-                          ));
-                    },
-                    child: Text('Aproveite'),
-                  ),
-                )
-              ],
-            ),
+    return Scaffold(
+      body: Container(
+        width: 1000,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover,
           ),
-        ));
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              'Bem-vinda(o)',
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CategoryKitchen(),
+                      ));
+                },
+                child: Text('Aproveite'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
